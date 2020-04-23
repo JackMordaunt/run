f53a6e4faa0e40f67634e597b257058edf0bea08
Jack Mordaunt <jackmordaunt@gmail.com>
2020-04-23 19:28:04 +0800

    Doc: Explain Config::from_args.

--- src/config.rs
+++ src/config.rs
@@ -7,6 +7,8 @@ pub struct Config {
 
 impl Config {
     /// Consumes a stream of strings and parses flags into config values.
+    /// Only actually consumes the values recognised by Config.
+    /// Returns on the first unrecognised value.
     pub fn from_args<Args, Str>(args: &mut Peekable<Args>) -> Self
     where
         Args: Iterator<Item = Str>,
