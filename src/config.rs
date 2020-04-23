8ebe2c45988953f1bf4d1f08d2b15f1140e0e7cd
Jack Mordaunt <jackmordaunt@gmail.com>
2020-04-23 19:23:55 +0800

    Feat: --dry-run flag. Fix: Pipeline parsing.

---
+++ src/config.rs
@@ -0,0 +1,30 @@
+use std::iter::Peekable;
+
+#[derive(Default, Debug)]
+pub struct Config {
+    pub dry_run: bool,
+}
+
+impl Config {
+    /// Consumes a stream of strings and parses flags into config values.
+    pub fn from_args<Args, Str>(args: &mut Peekable<Args>) -> Self
+    where
+        Args: Iterator<Item = Str>,
+        Str: AsRef<str>,
+    {
+        let mut config = Config::default();
+
+        while let Some(arg) = args.peek() {
+            match arg.as_ref() {
+                "--dry-run" | "--dry" => {
+                    config.dry_run = true;
+                }
+                _ => {
+                    break;
+                }
+            }
+            args.next();
+        }
+        config
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -14,10 +14,14 @@
 //! - Shell interface (basically, a loop with a prompt).
 //!
 
+mod config;
+
+use config::Config;
 use glob::glob;
 use std::collections::HashMap;
 use std::env;
 use std::error::Error;
+use std::fmt;
 use std::fs::File;
 use std::io::prelude::*;
 use std::process::{Child, Command, Stdio};
@@ -35,93 +39,134 @@ fn main() {
         if !run_file.ends_with(".run") {
             run_file.push_str(".run");
         }
-        File::open(run_file)
-            .expect("opening run file")
+        File::open(&run_file)
+            .map_err(|e| format!("opening {}: {}", &run_file, e))
+            .unwrap()
             .read_to_string(&mut file)
             .expect("reading run file");
     }
 
+    // Consume any config flags we care about.
+    let config = Config::from_args(&mut args);
+
+    if cfg!(debug_assertions) {
+        dbg!(&config);
+    }
+
     // Wrap each unique argument in quotes for the environment parser.
+    // Quotes get stripped on entry, so we add them back.
     let s: String = args.fold(String::new(), |mut buf, next| {
-        buf.push(' ');
         buf.push('"');
         buf.extend(next.chars());
         buf.push('"');
+        buf.push(' ');
         buf
     });
+
+    if cfg!(debug_assertions) {
+        dbg!(&s);
+    }
 
     let environment: Environment = s
         .parse()
         .map_err(|e| format!("parsing environment: {}", e))
         .unwrap();
 
+    if cfg!(debug_assertions) {
+        dbg!(&environment);
+    }
+
     let items = ItemParser { env: &environment }
         .parse(&file)
         .map_err(|e| format!("parsing commands: {}", e))
         .unwrap();
 
-    for item in items {
-        match item {
-            Item::Comment(comment) => {
-                println!("{}", comment);
-            }
-            Item::Pipeline { cmds, literal } => {
-                println!("{}", literal);
+    if cfg!(debug_assertions) {
+        dbg!(&items);
+    }
 
-                let mut prev = None;
-                let mut cmds = cmds.iter().peekable();
-
-                while let Some(Cmd { name, args }) = cmds.next() {
-                    // Note(jfm):
-                    //  Should builtins get access to pipes? Do they need them?
-                    //  Should we check to see if an "rm" utility exists on the machine?
-                    //  User would probably like to use their installed rm utitliy.
-                    match name.as_ref() {
-                        "rm" => {
-                            if let Err(err) = args
-                                .iter()
-                                .map(|arg| rm(arg))
-                                .collect::<Result<Vec<_>, _>>()
-                            {
-                                panic!("rm {}: {}", args.join(" "), err);
-                            }
-                        }
-                        _ => {
-                            let stdin = prev.map_or(Stdio::inherit(), |output: Child| {
-                                Stdio::from(output.stdout.unwrap())
-                            });
-                            let stdout = if cmds.peek().is_some() {
-                                Stdio::piped()
-                            } else {
-                                Stdio::inherit()
-                            };
-                            let output = Command::new(name)
-                                .current_dir(
-                                    env::current_dir().expect("fetching current working directory"),
-                                )
-                                .args(args)
-                                .stdin(stdin)
-                                .stdout(stdout)
-                                .spawn();
-                            match output {
-                                Ok(output) => prev = Some(output),
-                                Err(err) => {
-                                    panic!("{}: {}", name, err);
-                                }
-                            };
-                        }
-                    };
+    if config.dry_run {
+        for item in items {
+            match item {
+                Item::Comment(comment) => {
+                    println!("{}", comment);
                 }
-
-                if let Some(mut finish) = prev {
-                    finish.wait().ok();
+                Item::Pipeline {
+                    cmds,
+                    literal: _literal,
+                } => {
+                    for cmd in cmds {
+                        println!("{}", &cmd);
+                    }
+                }
+            };
+        }
+    } else {
+        for item in items {
+            match item {
+                Item::Comment(comment) => {
+                    println!("{}", comment);
+                }
+                Item::Pipeline {
+                    cmds,
+                    literal: _literal,
+                } => {
+                    let mut prev = None;
+                    let mut cmds = cmds.iter().peekable();
+                    while let Some(cmd) = cmds.next() {
+                        println!("{}", &cmd);
+                        let Cmd { name, args } = cmd;
+                        // Note(jfm):
+                        //  Should builtins get access to pipes? Do they need them?
+                        //  Should we check to see if an "rm" utility exists on the machine?
+                        //  User would probably like to use their installed rm utitliy.
+                        match name.as_ref() {
+                            "rm" => {
+                                if let Err(err) = args
+                                    .iter()
+                                    .map(|arg| rm(arg))
+                                    .collect::<Result<Vec<_>, _>>()
+                                {
+                                    panic!("rm {}: {}", args.join(" "), err);
+                                }
+                            }
+                            _ => {
+                                let stdin = prev.map_or(Stdio::inherit(), |output: Child| {
+                                    Stdio::from(output.stdout.unwrap())
+                                });
+                                let stdout = if cmds.peek().is_some() {
+                                    Stdio::piped()
+                                } else {
+                                    Stdio::inherit()
+                                };
+                                let output = Command::new(name)
+                                    .current_dir(
+                                        env::current_dir()
+                                            .expect("fetching current working directory"),
+                                    )
+                                    .args(args)
+                                    .stdin(stdin)
+                                    .stdout(stdout)
+                                    .spawn();
+                                match output {
+                                    Ok(output) => prev = Some(output),
+                                    Err(err) => {
+                                        panic!("{}: {}", name, err);
+                                    }
+                                };
+                            }
+                        };
+                    }
+                    if let Some(mut finish) = prev {
+                        finish.wait().ok();
+                    }
                 }
             }
         }
     }
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, Default, PartialEq)]
 struct Environment {
     named: HashMap<String, String>,
     positional: Vec<String>,
@@ -147,7 +192,6 @@ impl FromStr for Environment {
             if arg.starts_with("-") {
                 let name = arg.trim_matches('-').to_owned();
                 if let Some(value) = iter.next() {
-                    dbg!(&value);
                     if value.starts_with("-") {
                         return Err(format!("{} is missing a value", arg).into());
                     }
@@ -188,8 +232,8 @@ impl<'a> ItemParser<'a> {
                     Ok(vec![Item::Comment(s.into())])
                 } else {
                     s.split(";")
-                        .map(|s| s.split("|"))
-                        .flatten()
+                        // .map(|s| s.split("|"))
+                        // .flatten()
                         .map(|s| self.parse_pipeline(s))
                         .collect()
                 }
@@ -200,63 +244,66 @@ impl<'a> ItemParser<'a> {
             .collect())
     }
     // Parse a pipeline of commands into a pipeline structure.
+    // "cat src/main.rs | rg match | head 20"
     fn parse_pipeline(&self, fragment: &str) -> Result<Item, String> {
-        let mut cmds: Vec<Cmd> = vec![];
-        let mut words = SplitWords {
-            src: fragment.chars(),
-        };
-
-        if let Some(name) = words.next() {
-            let cmd = Cmd {
-                name: name.to_owned(),
-                args: words
-                    .map(String::from)
-                    .map(|arg| {
-                        // Basically, if arg is "$<numeric>" we parse
-                        // the number and lookup the corresponding positional argument.
-                        // If arg is "$<identifier>" we lookup the named argument.
-                        // If either one doesn't exist we throw up an error.
-
-                        // TODO(jfm): -Version 0.3.0 + "v$Version" -> "v0.3.0"
-
-                        if arg.contains('$') {
-                            if let Ok(index) = arg
-                                .chars()
-                                .skip(1)
-                                .next()
-                                .unwrap()
-                                .to_string()
-                                .parse::<usize>()
-                            {
-                                match self.env.positional.get(index - 1) {
-                                    Some(v) => Ok(v.to_owned()),
-                                    None => {
-                                        Err(format!("no positional argument given for ${}", index))
+        let cmds = fragment
+            .split("|")
+            .map(|s| SplitWords { src: s.chars() })
+            .map(|mut words| -> Result<Cmd, String> {
+                if let Some(name) = words.next() {
+                    let cmd = Cmd {
+                        name: name.to_owned(),
+                        args: words
+                            .map(String::from)
+                            .map(|arg| {
+                                // Basically, if arg is "$<numeric>" we parse
+                                // the number and lookup the corresponding positional argument.
+                                // If arg is "$<identifier>" we lookup the named argument.
+                                // If either one doesn't exist we throw up an error.
+                                // TODO(jfm): -Version 0.3.0 + "v$Version" -> "v0.3.0"
+                                if arg.contains('$') {
+                                    if let Ok(index) = arg
+                                        .chars()
+                                        .skip(1)
+                                        .next()
+                                        .unwrap()
+                                        .to_string()
+                                        .parse::<usize>()
+                                    {
+                                        match self.env.positional.get(index - 1) {
+                                            Some(v) => Ok(v.to_owned()),
+                                            None => Err(format!(
+                                                "no positional argument given for ${}",
+                                                index
+                                            )),
+                                        }
+                                    } else {
+                                        let mut parts = arg.split('$');
+                                        let prefix = parts.next().unwrap();
+                                        let index = parts.next().unwrap().trim();
+                                        match self.env.named.get(index) {
+                                            Some(v) => Ok(format!("{}{}", prefix, v)),
+                                            None => Err(format!(
+                                                "no value specified for named argument: {}",
+                                                index,
+                                            )),
+                                        }
                                     }
+                                } else {
+                                    Ok(arg)
                                 }
-                            } else {
-                                let mut parts = arg.split('$');
-                                let prefix = parts.next().unwrap().trim();
-                                let index = parts.next().unwrap().trim();
-                                match self.env.named.get(index) {
-                                    Some(v) => Ok(format!("{}{}", prefix, v)),
-                                    None => Err(format!(
-                                        "no value specified for named argument: {}",
-                                        index,
-                                    )),
-                                }
-                            }
-                        } else {
-                            Ok(arg)
-                        }
-                    })
-                    .collect::<Result<Vec<_>, _>>()?,
-            };
-            cmds.push(cmd);
-        }
+                            })
+                            .collect::<Result<Vec<_>, _>>()?,
+                    };
+                    Ok(cmd)
+                } else {
+                    Err("empty command".into())
+                }
+            })
+            .collect::<Result<Vec<_>, _>>()?;
 
         Ok(Item::Pipeline {
-            cmds: cmds,
+            cmds,
             literal: fragment.trim().into(),
         })
     }
@@ -272,6 +319,13 @@ struct Cmd {
 enum Item {
     Comment(String),
     Pipeline { cmds: Vec<Cmd>, literal: String },
+}
+
+impl fmt::Display for Cmd {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{} {}", self.name, self.args.join(" "))?;
+        Ok(())
+    }
 }
 
 // rm the given glob pattern.
@@ -341,7 +395,7 @@ mod tests {
             {
                 let mut m = ::std::collections::HashMap::new();
                 $(
-                    m.insert($key, $value);
+                    m.insert($key.into(), $value.into());
                 )+
                 m
             }
@@ -391,12 +445,43 @@ mod tests {
         }];
         let got = ItemParser {
             env: &Environment {
-                named: map! {"Version".to_owned() => "0.3.0".to_owned()},
+                named: map! {"Version" => "0.3.0"},
                 positional: vec![],
             },
         }
         .parse(&input)
         .unwrap();
         assert_eq!(got, want);
+    }
+
+    #[test]
+    fn test_pipeline_parsing() {
+        let input = r#"cat src/main.rs | rg match | head 5"#;
+        let want = vec![
+            Cmd {
+                name: "cat".into(),
+                args: vec!["src/main.rs".into()],
+            },
+            Cmd {
+                name: "rg".into(),
+                args: vec!["match".into()],
+            },
+            Cmd {
+                name: "head".into(),
+                args: vec!["5".into()],
+            },
+        ];
+        let got = ItemParser {
+            env: &Environment::default(),
+        }
+        .parse(&input)
+        .expect("parsing");
+        assert_eq!(
+            got,
+            vec![Item::Pipeline {
+                cmds: want,
+                literal: input.into()
+            }]
+        );
     }
 }
